02241997a004d950a46aff51480951c14e566df7
criccoder45 <154867942+criccoder45@users.noreply.github.com>
2024-03-29 16:18:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 let movies = [
   {
-    name: "loki",
-    des: "Loki is an American television series created by Michael Waldron for the streaming service Disney.",
-    image: "./images/slider 1.PNG",
+    name: "CRIC CODER",
+    des: "On This Sports Web, You Can Watch All Most Every Sports Channel You Want.",
+    image: "https://i.ibb.co/7WXBV82/Fxrolx-Pa-AAArbwh.jpg",
   },
   {
-    name: "falcon and the winter soldier",
-    des: "Falcon and the Winter Soldier is an American television series created for the streaming platform Disney+.",
-    image: "./images/slider 2.PNG",
+    name: "CRIC CODER",
+    des: "On This Sports Web, You Can Watch All Most Every Sports Channel You Want.",
+    image: "https://i.ibb.co/Q97v7Gw/maxresdefault-1.jpg",
   },
   {
-    name: "WandaVision",
-    des: "WandaVision is an American television series created for the streaming service Disney+",
-    image: "./images/slider 3.PNG",
+    name: "CRIC CODER",
+    des: "On This Sports Web, You Can Watch All Most Every Sports Channel You Want.",
+    image: "https://i.ibb.co/HgGKTWj/GBs-B-L-a-MAAX4g-N.jpg",
   },
-
-  {
-    name: "Raya and the Last Dragon",
-    des: "Raya and the Last Dragon is an animated Disney film released in 2021.",
-    image: "./images/slider 4.PNG",
-  },
-
-  {
-    name: "Luca",
-    des: "Luca is a Disney-Pixar animated film released in 2021.",
-    image: "./images/slider 5.PNG",
-  },
+  
+  
 ];
 
 const carousel = document.querySelector(".carousel");
